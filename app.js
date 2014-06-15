aad83f38d84bb64b29586fa6dbdd74983a8f9d4f
sorpa'as plat <sorpaas@gmail.com>
2014-06-15 19:02:35 +0800

    load database into app

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var server = require('http').createServer(app)
 var io = require('socket.io').listen(server);
 var device = require('express-device');
 var routes = require('./routes');
+var mongo = require('mongojs');
 
 var runningPortNumber = process.env.PORT || 5678;
+var databaseUrl = process.env.DATABASE_URL;
+var databaseCollections = ["sessions", "users", "groups", "topics", "comments", "notifications"];
+var db = mongo.connect(databaseUrl, databaseCollections);
 
 routes(app);
 sockets(io);
